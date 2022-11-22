da3c8bd0343ddf41441bf68641e59038bdada580
zack <rubinzack40@gmail.com>
2022-11-21 23:19:41 -0500

    added bidirectional stream server code

--- server/message.go
+++ server/message.go
@@ -2,12 +2,39 @@ package server
 
 import (
 	"context"
+	"io"
 
 	pb "github.com/zscrub/rchat_server/protos"
 )
 
 func (s *RouteServer) SendMessage(ctx context.Context, message *pb.Message) (*pb.Response, error) {
 	// store message
+	return &pb.Response{Detail: "Connected"}, nil
+}
 
-	return &pb.Response{Detail: "hey lol"}, nil
+func (s *RouteServer) ChatSession(stream pb.RChat_ChatSessionServer) error {
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		// key := serialize(in.Content)
+		key := in.Content
+
+		s.mu.Lock()
+		s.routeNotes[key] = append(s.routeNotes[key], in)
+
+		rn := make([]*pb.RouteNote, len(s.routeNotes[key]))
+		copy(rn, s.routeNotes[key])
+		s.mu.Unlock()
+
+		for _, note := range s.routeNotes[key] {
+			if err := stream.Send(note); err != nil {
+				return err
+			}
+		}
+	}
 }
--- server/struct.go
+++ server/struct.go
@@ -1,9 +1,14 @@
 package server
 
 import (
+	"sync"
+
 	pb "github.com/zscrub/rchat_server/protos"
 )
 
 type RouteServer struct {
 	pb.UnimplementedRChatServer
+
+	mu         sync.Mutex // protects routeNotes
+	routeNotes map[string][]*pb.RouteNote
 }
